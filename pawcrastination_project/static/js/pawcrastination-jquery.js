38b2dc075f75a9c038f834079def09cf3ec4f5c4
thenipplephilosophy <cameronrsbh@googlemail.com>
2015-03-27 10:51:25 +0000

    fixed the bug for jquery in index
    why does fixing a bug make a bug, waa.

--- pawcrastination_project/static/js/pawcrastination-jquery.js
+++ pawcrastination_project/static/js/pawcrastination-jquery.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function($) {
 	jQuery('#wrapper3').hide();
 	jQuery('.picture-pop').click( function(event) {
-		window.open($(this).attr('href2') + $(this).attr('href1'),'_blank','height=600,width=650');
+		//window.open($(this).attr('href2') + $(this).attr('href1'),'_blank','height=600,width=650');
+        window.open($(this).attr('href1'),'_blank','height=600,width=650');
 		
 	});
 
